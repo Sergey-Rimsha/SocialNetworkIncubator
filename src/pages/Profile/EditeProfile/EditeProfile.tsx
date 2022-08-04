8fb982f9d2b46089e629be46d0dd9520773f5285
Sergey.rimsha.ru@gmail.com <Sergey.rimsha.ru@gmail.com>
2022-08-04 22:04:22 +0300

    add formik EditeProfile

--- src/pages/Profile/EditeProfile/EditeProfile.tsx
+++ src/pages/Profile/EditeProfile/EditeProfile.tsx
@@ -4,12 +4,52 @@ import {AppRootStateType} from "../../../store/store";
 import {ContactsType} from "../../../store/reducers/profileReducer";
 
 import s from './EditeProfile.module.scss';
+import {useFormik} from "formik";
+
+export type FormikErrorType = {
+
+}
 
 
 export const EditeProfile = () => {
 
 	const contacts = useSelector<AppRootStateType, ContactsType>(state => state.profilePage.user.contacts);
 
+	const formik = useFormik({
+		initialValues: {
+			github: '',
+			vk: '',
+			facebook: '',
+			instagram: '',
+			twitter: '',
+			website: '',
+			youtube: '',
+			mainLink: '',
+			...contacts
+		},
+		validate: (values) => {
+			const errors: FormikErrorType = {};
+			// if (!values.email) {
+			// 	errors.email = 'Required';
+			// } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+			// 	errors.email = 'Invalid email address';
+			// }
+
+			// if (!values.password) {
+			// 	errors.password = 'Пароль обязателен';
+			// } else if (values.password.length < 3) {
+			// 	errors.password = 'Пароль должен быть больше 3 символов';
+			// }
+
+			return errors;
+		},
+		onSubmit: values => {
+			// props.onHandlerSubmit(values)
+			console.log(values)
+			formik.resetForm();
+		},
+
+	});
 
 	return (
 		<div className={s.editeProfile}>
@@ -21,22 +61,26 @@ export const EditeProfile = () => {
 					<div className={s.editeProfile__subTitle}>
 						Contacts:
 					</div>
-					{
-						Object.keys(contacts).map((key, i) => {
-							return (
-								<>
-									<div className={s.editeProfile__contact}>
-										{key}:
+					<form onSubmit={formik.handleSubmit}>
+						{
+							Object.keys(contacts).map((key, i) => {
+								return (
+									<div key={i}>
+										<span className={s.editeProfile__contact}>
+											{key}:
+										</span>
+										<input
+											placeholder={key}
+											{...formik.getFieldProps(`${key}`)}
+										/>
 									</div>
-									<input name={key} placeholder={key} />
-								</>
-							)
-						})
-					}
-
+								)
+							})
+						}
+						<button type='submit'>save</button>
+					</form>
 				</div>
 			</div>
-
 		</div>
 	)
 }
